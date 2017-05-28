15ee993f1df5282e2068723f3f41c55cb09ea5ce
Akaame <sddkacil.1@gmail.com>
2017-05-28 19:08:58 +0300

    Added sampler classes which derives from ISampler interface. ReservoirSampler implements IReservoirSampler which implements ISampler. Each Sampler object is instantiated with a list of data. Indistinct sampler basically returns a random list of indistinct elements(As in rangeintprovider). Reservoir sampler gets a list of data and apply Reservoir Sampling algorithm which is GUARANTEED to return DISTINCT samples IF ONLY input data is distinct. And it does not randomize over sorted data. Things to do -> Shuffling outputs after reservoir sampling.  -> Getting rid of duplicate elements in input

---
+++ test/DataSuit.Tests/SamplerTests.cs
@@ -0,0 +1,81 @@
+using System;
+using Xunit;
+using DataSuit.Infrastructures;
+using System.Collections.Generic;
+using DataSuit.Enums;
+using Xunit.Abstractions;
+using DataSuit.Interfaces;
+
+namespace DataSuit.Tests
+{
+
+    public class SamplerTests
+    {
+        // Mock Testing would cover sampling scenarios better IMO
+        // Test normal sampling case
+        [Fact]
+        public void testRandomIndistinct()
+        {
+            int min = 10, max = 20;
+
+            List<int> sampleTarget = new List<int>();
+            for( int i = min ; i < max ; i++)
+            {
+                sampleTarget.Add(i);
+            }
+
+            ISampler<int>  sampler = new IndistinctSampler<int>(sampleTarget);
+            var sampled = sampler.sample(3);
+
+            foreach(var sample in sampled)
+            {
+                Assert.True(sampleTarget.Contains(sample), $"Value of sample is {sample}");
+            }
+        }
+
+        //Test when sample size is greater than or equal to data size
+        [Fact]
+        public void testRandomIndistinctPopulated()
+        {
+            int min = 10, max = 20;
+
+            List<int> sampleTarget = new List<int>();
+            for( int i = min ; i < max ; i++)
+            {
+                sampleTarget.Add(i);
+            }
+
+            ISampler<int>  sampler = new IndistinctSampler<int>(sampleTarget);
+            var sampled = sampler.sample(max - min+10);
+
+            foreach(var sample in sampled)
+            {
+                Assert.True(sampleTarget.Contains(sample), $"Value of sample is {sample}");
+            }
+        }
+
+        [Fact]
+        public void testRandomReservoir()
+        {
+            int min = 10, max = 20;
+            bool[] hit = new bool[max-min];
+
+            List<int> sampleTarget = new List<int>();
+            for( int i = min ; i < max ; i++)
+            {
+                sampleTarget.Add(i);
+            }
+
+            ISampler<int>  sampler = new ReservoirSampler<int>(sampleTarget);
+            var sampled = sampler.sample(max - min -3 );
+
+            Console.WriteLine("Final Array");
+            foreach(var sample in sampled)
+            {
+                Assert.True(sampleTarget.Contains(sample), $"Value of sample is {sample}");
+                Assert.False(hit[sample-min]);
+                hit[sample-min] = true;
+            }
+        }
+    }
+}
