5f354eb166f430cf23b426be4d0275ed0c52cfae
lyzerk <ozdemirali@itu.edu.tr>
2018-03-14 23:30:41 +0300

    removed unnecessary codes

--- src/DataSuit/Infrastructures/Utility.cs
+++ src/DataSuit/Infrastructures/Utility.cs
@@ -12,19 +12,6 @@ namespace DataSuit.Infrastructures
     internal class Utility
     {
         public static Random Rand = new Random((int)DateTime.Now.Ticks);
-        public static HttpClient Client;
         public static readonly string Seperator = ",";
-        public static List<IMapping> Maps { get; set; } = new List<IMapping>();
-        public static List<IMapping> PendingMaps { get; set; } = new List<IMapping>();
-        
-        static Utility()
-        {
-            var ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
-            Client = new HttpClient(new HttpClientHandler
-            {
-                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip
-            });
-            Client.DefaultRequestHeaders.Add("User-Agent", ua);
-        }
     }
 }
